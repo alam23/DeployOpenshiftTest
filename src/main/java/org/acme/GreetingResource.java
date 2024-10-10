551da8c75708d77abdae52f936ac128335dff461
Anderson Alberto <amiranda@inspiratls.com>
2024-10-10 17:54:33 -0500

    Test 3

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -11,6 +11,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasyUpdate2";
+        return "Hello RESTEasyUpdate200";
     }
 }
