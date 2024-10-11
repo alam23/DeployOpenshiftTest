da8103e5b3dfff44717e014e6d0b68f945b819e8
Anderson Alberto <amiranda@inspiratls.com>
2024-10-10 20:01:40 -0500

    Test 11

--- src/test/java/org/acme/GreetingResourceTest.java
+++ src/test/java/org/acme/GreetingResourceTest.java
@@ -14,7 +14,7 @@ class GreetingResourceTest {
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("Hello RESTEasyUpdate"));
+             .body(is("Hello RESTEasyUpdate2024"));
     }
 
 }
