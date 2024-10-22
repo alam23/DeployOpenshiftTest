c5019d9951884b47d438a3c94d9856634b76bbbb
Anderson Alberto <amiranda@inspiratls.com>
2024-10-22 14:00:39 -0500

    Test openshift enviorment values

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,5 +1,7 @@
 package org.acme;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -8,9 +10,21 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
+    @ConfigProperty(name= "app.dev.name")
+    String devName;
+
+    @ConfigProperty(name = "app.project.name")
+    String projectName;
+
+    @ConfigProperty(name = "company")
+    String company;
+
+    @ConfigProperty(name = "secret.token")
+    String secretToken;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasyUpdate202";
+        return "Hello RESTEasy " + devName + " " + projectName + " " + company + " " + secretToken;
     }
 }
